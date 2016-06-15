305c3162b53a37dd1ae662d76024cdafc656bd33
Andrea D'Ippolito <adedip@gmail.com>
2016-06-15 17:24:48 +0200

    facebook login

--- server/server.js
+++ server/server.js
@@ -25,3 +25,19 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+// Make sure to also put this in `server/server.js`
+var PassportConfigurator =
+  require('loopback-component-passport').PassportConfigurator;
+ 
+// Include this in your 'facebook-oauth.js' boot script in `server/boot`.
+var passportConfigurator = new PassportConfigurator(app);
+ 
+passportConfigurator.init();
+passportConfigurator.setupModels({
+  userModel: app.models.User,
+  userIdentityModel: app.models.UserIdentity,
+  userCredentialModel: app.models.UserCredential
+});
+passportConfigurator.configureProvider('facebook-login',
+  require('../providers.json')['facebook-login']);
