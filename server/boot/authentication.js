5d30a30509b240bc7a8721ed148994a3e4038a56
Andrea D'Ippolito <adedip@gmail.com>
2016-07-11 11:48:24 +0200

    fixed facebook login

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,4 +1,8 @@
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
+  server.middleware('auth', server.loopback.token({
+    model: server.models.accessToken,
+    currentUserLiteral: 'me'
+  }));
 };
